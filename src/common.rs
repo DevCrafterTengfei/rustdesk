266ee6c3d42f52a0c717888627ae8848343442e3
DevCrafterTengfei <152072695+DevCrafterTengfei@users.noreply.github.com>
2024-09-14 08:33:05 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -900,7 +900,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://honeywellwang.mycloudnas.com".to_owned()
+    "http://60.12.94.218".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
