a607cefa8425f4d27b6c981c261df3a4b3f70a7d
DevCrafterTengfei <152072695+DevCrafterTengfei@users.noreply.github.com>
2024-09-29 21:42:38 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.shidi.vip"];
-pub const PUBLIC_RS_PUB_KEY: &str = "sxh39XA6RTqtgv0Qurf2YYb+UJaMH7kzCIqc3jCCVoI=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["149.88.75.236"];
+pub const PUBLIC_RS_PUB_KEY: &str = "TKbUZWvwMcKYUbJ5tVXfM5ZMm2nAl4HrciSbb4wGITw=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
