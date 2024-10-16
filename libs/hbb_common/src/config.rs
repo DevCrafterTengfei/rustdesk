e9a513fc6f927c2e8f8da699c920b843a35fb7d6
DevCrafterTengfei <152072695+DevCrafterTengfei@users.noreply.github.com>
2024-10-16 18:41:44 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["47.96.227.201"];
-pub const PUBLIC_RS_PUB_KEY: &str = "15325175268";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["121.62.29.80"];
+pub const PUBLIC_RS_PUB_KEY: &str = "0123456789";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
