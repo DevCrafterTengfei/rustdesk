b64c85f15e6fb220bb8c96caa4cad303df096618
DevCrafterTengfei <152072695+DevCrafterTengfei@users.noreply.github.com>
2024-11-13 10:25:14 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["43.129.159.104"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["124.70.14.76"];
 pub const PUBLIC_RS_PUB_KEY: &str = "0123456789";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
