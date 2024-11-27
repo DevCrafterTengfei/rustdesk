447c9e1024d3dde45ce50a8692f91476c51d9be0
DevCrafterTengfei <152072695+DevCrafterTengfei@users.noreply.github.com>
2024-11-27 10:44:34 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -900,7 +900,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://106.119.140.19:21114".to_owned()
+    "http://159.138.22.142:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
